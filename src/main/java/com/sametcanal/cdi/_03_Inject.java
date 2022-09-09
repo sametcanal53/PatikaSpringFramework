aa4432dd98be6967dd6b7f32cb4e69155a8753e6
Sametcan <sametcanal53@hotmail.com>
2022-09-10 01:59:03 +0300

    CDI Inject

---
+++ src/main/java/com/sametcanal/cdi/_03_Inject.java
@@ -0,0 +1,17 @@
+package com.sametcanal.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named(value = "injecttuto")
+@ApplicationScoped
+public class _03_Inject {
+    @Inject
+    List<String> tuketen;
+
+    public List<String> getTuketen() {
+        return tuketen;
+    }
+}
