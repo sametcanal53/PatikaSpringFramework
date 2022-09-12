4289080f5a3d29e2d6c283c8ca9f25918717810c
Sametcan <sametcanal53@hotmail.com>
2022-09-12 17:00:49 +0300

    Scoped Annotation (ApplicationScoped-RequestScoped-SessionScoped-ConversationScoped)

---
+++ src/main/java/com/sametcanal/cdi/_00_Scoped.java
@@ -0,0 +1,16 @@
+package com.sametcanal.cdi;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+// kapsam
+// yaşam süresi
+// performans
+@Named
+//@ApplicationScoped - bütün uygulama boyunca çalışır
+//@RequestScoped - 1 istek boyunca çalışır
+//@SessionScoped - bir kullanıcı için çalışır
+//@Dependent - bukalemun
+//@ConversationScoped - belli istek boyunca çalışır.
+public class _00_Scoped {
+}
